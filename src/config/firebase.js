fab8eaefb5170ed8ce56888fbbf14503b31c6b68
Developer Sid <142804820+Siddarth-abcs@users.noreply.github.com>
2023-10-06 11:02:57 +0530

    done

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,13 +5,13 @@ import "firebase/compat/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBazQ7sknQ8zn53PDQTLPQyXW4BiLXgaoE",
-    authDomain: "datasystem-4ab3d.firebaseapp.com",
-    databaseURL: "https://datasystem-4ab3d-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "datasystem-4ab3d",
-    storageBucket: "datasystem-4ab3d.appspot.com",
-    messagingSenderId: "761286626969",
-    appId: "1:761286626969:web:85fb7dace68a93340eb208"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    databaseURL: import.meta.env.VITE_databaseURL,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
   };
 
   const fire = firebase.initializeApp(firebaseConfig);
