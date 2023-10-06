89bacd2995b9c6354441df8ccc16044f8ba2d8a0
Developer Sid <142804820+Siddarth-abcs@users.noreply.github.com>
2023-10-06 13:01:49 +0530

    Delete src directory

--- src/pages/Homepage/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Login } from "../AuthPage/Login/Login";
-export { default as Register } from "../AuthPage/Register/Register";
-export { default as DashboardPage } from "../Dashboardpage/Dashboardpage";
-export { default as Homepage } from "./Homepage";
